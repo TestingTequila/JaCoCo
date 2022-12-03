4f715fc01003909126adc0c73f597713f899f547
Ashish Mishra <ashish.mishra.sse@gmail.com>
2022-12-04 00:12:14 +0530

    First Commit

---
+++ src/main/java/JaCoCo/JaCoCo/App.java
@@ -0,0 +1,13 @@
+package JaCoCo.JaCoCo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/stepDefinitions/JacocoTesting.java
@@ -0,0 +1,19 @@
+package stepDefinitions;
+
+import org.testng.annotations.Test;
+
+public class JacocoTesting {
+
+	@Test
+	public void test1()
+	{
+		System.out.println("This is test 1");
+	}
+	
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("This is test 2");
+	}
+}
