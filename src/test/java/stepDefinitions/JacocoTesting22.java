9fb9ddaaaf9053e75f9831a4c9756df122ae68e1
Ashish Mishra <ashish.mishra.sse@gmail.com>
2022-12-04 01:13:26 +0530

    Commiting JacocoTesting22 to BranchX

---
+++ src/test/java/stepDefinitions/JacocoTesting22.java
@@ -0,0 +1,19 @@
+package stepDefinitions;
+
+import org.testng.annotations.Test;
+
+public class JacocoTesting22 {
+
+	@Test
+	public void test11()
+	{
+		System.out.println("This is test 1");
+	}
+	
+	
+	@Test
+	public void test22()
+	{
+		System.out.println("This is test 2");
+	}
+}
